0545638c4aa739dfd8bcbf7c647e047a56474034
Hilary Fung <hfung@seattletimes.com>
2020-03-30 15:51:52 -0700

    styles and interaction!

--- src/js/main.js
+++ src/js/main.js
@@ -2,3 +2,28 @@
 // setTimeout(() => paywall(12345678), 5000);
 
 require("component-responsive-frame/child");
+
+const rows = document.querySelectorAll(".basic-info");
+
+rows.forEach(function(row) {
+	row.addEventListener("click", function(e) {
+		if (this.classList.contains("collapsed")) {
+			this.classList.remove("collapsed");
+			this.classList.add("expanded");
+		} else {
+			this.classList.remove("expanded");
+			this.classList.add("collapsed");
+		}
+
+		let question = this.dataset.question;
+		let answer = document.querySelector("[data-answer='" + question + "']");
+
+		if (answer.classList.contains("hidden")) {
+			answer.classList.remove("hidden");
+			answer.classList.add("shown");
+		} else {
+			answer.classList.remove("shown");
+			answer.classList.add("hidden");
+		}
+	});
+});
